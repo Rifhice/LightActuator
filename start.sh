84ac50a5e8904cd726af427310a0cc8075866f62
Rifhice <giordani.kevin.02@gmail.com>
2018-08-25 01:28:41 +0000

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+processId=$(ps -ef | grep 'indexLight' | grep -v 'grep' | awk '{ printf $2 }')
+sudo kill -9 $processId
 cd /home/pi/Desktop/Hom-E_LightActuator/
 git pull
-sudo node index.js
+node indexLight.js
